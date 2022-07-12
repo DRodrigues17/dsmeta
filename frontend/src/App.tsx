db2d304ddc654b94b699dd08b5fc00e94557dff9
DRodrigues17 <adanielluiz71@gmail.com>
2022-07-12 15:05:36 -0300

    primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
+import NotificationButton from "./components/NotificationButtton"
+
 function App() {
- return (
-  <h1>daniel dev</h1>
- )
- 
+  return (
+    <>
+      <h1>daniel dev</h1>
+      <NotificationButton />
+    </>
+
+  )
+
 }
 
 export default App
